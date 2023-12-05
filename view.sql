6bbd57f97d8c79ec13953518cc3a1b923018f5a5
kostas2370 <96636678+kostas2370@users.noreply.github.com>
2023-12-05 19:12:55 +0200

    xd

--- view.sql
+++ view.sql
@@ -1,4 +1,14 @@
-CREATE VIEW `car_sales_view` AS  
-    SELECT Car.Model, Car.Weight, Car.Price , Manufacturer.Name,Manufacturer.Country, count(Sale.ID) FROM Car INNER JOIN Manufacturer ON Car.Manufacturer_ID = Manufacturer.ID INNER JOIN Sale on Car.ID = Sale.Car_ID GROUP BY Car.ID;
-    
-
+CREATE view `car_sales_view`
+AS
+  SELECT Car.model,
+         Car.weight,
+         Car.price,
+         Manufacturer.name,
+         Manufacturer.country,
+         Count(Sale.id)
+  FROM   Car
+         INNER JOIN Manufacturer
+                 ON Car.manufacturer_id = Manufacturer.id
+         INNER JOIN Sale
+                 ON Car.id = Sale.car_id
+  GROUP  BY Car.id; 
